99b0dfa5676b1013ef1f2fcffe152a7425d271c2
Sergiu Lungu <sergiu.lungu@opentech.email>
2023-11-08 15:23:01 +0200

    test

--- src/routes/back-office.jsx
+++ src/routes/back-office.jsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 
-import Userfront from '@userfront/toolkit/react';
-
 export default function Backoffice() {
   return (
-    
-    <div >
-        <h1>backoffice</h1>
-        <button onClick={Userfront.logout}>Logout</button>
+    <div>
+      <h1>backoffice</h1>
     </div>
   );
 }
