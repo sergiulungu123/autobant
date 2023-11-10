b829f941592b0034aa97a41df653a42c5600170a
Sergiu Lungu <sergiu.lungu@opentech.email>
2023-11-10 16:34:17 +0200

    feat: add language switch

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,11 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
 
 import cls from '../style.module.css';
+import { useTranslation } from 'react-i18next';
 
 function Main() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
   return (
-    <div className={cls.app}>
-      <p className={cls.loading}>{process.env.REACT_APP_MESSAGE}</p>
+    <div>
+      <button
+        class="btn btn-light"
+        onClick={() => changeLanguage('ro')}
+      >
+        eng
+      </button>
+      <button
+        class="btn btn-light"
+        onClick={() => changeLanguage('rus')}
+      >
+        rus
+      </button>
+      <div className={cls.app}>
+        <p className={cls.loading}> {t('progress')}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import './index.css';
+
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import common_ro from './language/ro/translation.json'
+import common_rus from './language/rus/translation.json';
+import i18n from 'i18next';
 import reportWebVitals from './reportWebVitals';
 
+i18n
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      ro: {
+        translation: common_ro,
+      },
+      rus: {
+        translation: common_rus,
+      },
+    },
+    fallbackLng: 'ro',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
