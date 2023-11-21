a369c2b81976280cb9507e1c68337ea477fa0c78
Sergiu Lungu <sergiu.lungu@opentech.email>
2023-11-21 08:57:52 +0200

    fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"01OuM0L7/2CRqh5WKHjn8QaKOl9tl6Jtbi/w+rWQ24I=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vs8ywBEHh866hFY2s7CnfNe6/MCFKEdVvJH95LKOuRs=\"\n}"
