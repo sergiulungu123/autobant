8273777431377d9cd262a1cf27491ba4d650ab90
Sergiu Lungu <sergiu.lungu@opentech.email>
2023-12-04 01:32:56 +0200

    feat: change icons sm

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 import { useTranslations } from 'next-intl';
-import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
 import styles from './main.module.css'; // Import the CSS module
-import Head from 'next/head';
+import {
+  FacebookIcon,
+  InstagramIcon,
+} from 'next-share';
 
 export default function ComingSoon() {
   const t = useTranslations('Home');
@@ -15,13 +17,13 @@ export default function ComingSoon() {
       <div className={styles.centeredText}>
         <h3>{t('under-construction')}</h3>
         <h3>{t('title')}</h3>
-        <div className={styles.linkStyle}>
-        <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Fautobant.md%3Figshid%3DM2RkZGJiMzhjOQ%253D%253D%26fbclid%3DIwAR1AAi_SOYlUbCee1Js23Rf5qjfyaOAjy1nl6YoW_ZOCAvCyCE70bBso7AE&h=AT2ZEhalXhdauTZZ_brz6IclSpaIyb51zpN6Nh0eOtSKm3-DNP4_bl3LMnklSZtrTqtIEby1tPrVJKfspue8gdDQTa6wIdG_32WNHIeLhU1QxpeUkMq_KfDUzMODIU6KtlVae7h5z-E">
-          <FaInstagram />
-        </a>
-        <a href="https://www.facebook.com/autobantMD?mibextid=LQQJ4d" >
-          <FaFacebook />
-        </a>
+        <div >
+          <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Fautobant.md%3Figshid%3DM2RkZGJiMzhjOQ%253D%253D%26fbclid%3DIwAR1AAi_SOYlUbCee1Js23Rf5qjfyaOAjy1nl6YoW_ZOCAvCyCE70bBso7AE&h=AT2ZEhalXhdauTZZ_brz6IclSpaIyb51zpN6Nh0eOtSKm3-DNP4_bl3LMnklSZtrTqtIEby1tPrVJKfspue8gdDQTa6wIdG_32WNHIeLhU1QxpeUkMq_KfDUzMODIU6KtlVae7h5z-E">
+            <InstagramIcon />
+          </a>
+          <a href="https://www.facebook.com/autobantMD?mibextid=LQQJ4d">
+            <FacebookIcon />
+          </a>
         </div>
       </div>
     </div>
